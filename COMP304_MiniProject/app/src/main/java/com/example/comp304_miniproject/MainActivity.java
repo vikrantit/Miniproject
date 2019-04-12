51d7d43cc2755e724b6d7c747a5dddbe155eba79
vikrantit <47832146+vikrantit@users.noreply.github.com>
2019-04-12 12:25:06 -0400

    Added and made changes in layout

---
+++ COMP304_MiniProject/app/src/main/java/com/example/comp304_miniproject/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.comp304_miniproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void sendMessage(View view) {
+        // Do something in response to button
+    }
+}
