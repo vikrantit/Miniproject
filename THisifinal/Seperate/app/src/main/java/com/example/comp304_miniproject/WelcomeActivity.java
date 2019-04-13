60fac0384b3f82c7e366fdded9033b651d8d5eea
vikrantit <47832146+vikrantit@users.noreply.github.com>
2019-04-13 13:59:50 -0400

    testing

--- THisifinal/Seperate/app/src/main/java/com/example/comp304_miniproject/WelcomeActivity.java
+++ THisifinal/Seperate/app/src/main/java/com/example/comp304_miniproject/WelcomeActivity.java
@@ -16,7 +16,8 @@ public class WelcomeActivity extends AppCompatActivity {
         //prepare a new layout for welcome activity(after login)
         //3 buttons : one for posting ajob
           //      one for checking available jobs
-            //    and one for previous applied jobs
+            //    and one for previous applied jobs continue
+        //
 
     }
 }
