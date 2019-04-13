8f98718dd0b16f1ff9f231b541304fccd24b83d6
Subhankar1909 <31899275+Subhankar1909@users.noreply.github.com>
2019-04-13 13:00:23 -0400

    Added the new updated version of the project

---
+++ Seperate/Seperate/app/src/main/java/com/example/comp304_miniproject/User.java
@@ -0,0 +1,65 @@
+package com.example.comp304_miniproject;
+
+public class User {
+
+    private int id;
+    private String username;
+    private String emailid;
+    private String password;
+    private String phoneno;
+
+    public User(int id, String username, String emailid, String password, String phoneno) {
+        this.id = id;
+        this.username = username;
+        this.emailid = emailid;
+        this.password = password;
+        this.phoneno = phoneno;
+    }
+    public User(){
+
+    }
+
+
+
+
+
+    public String getPhoneno() {
+        return phoneno;
+    }
+
+    public void setPhoneno(String phoneno) {
+        this.phoneno = phoneno;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmailid() {
+        return emailid;
+    }
+
+    public void setEmailid(String emailid) {
+        this.emailid = emailid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
